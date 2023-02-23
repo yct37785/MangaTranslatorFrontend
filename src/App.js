5296a0a6436a807c55b11deb6246e8e4978a0f47
lumberman88 <yiechertan@gmail.com>
2023-02-23 11:20:56 +0800

    MUI lib and sample UIs.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-vertical-page align-center">
+        <div className='vertical-orientation'>
+          <TextField id="outlined-basic" label="URL" variant="outlined" style={{ width: '500px' }} />
+          <Typography variant="body1" gutterBottom>
+            Supported URLs: Mangadex, RawKuma
+          </Typography>
+        </div>
+      </div>
     </div>
   );
 }
