85e7a0e101c39f20ceeadbd5c908ef098ed51241
lumberman88 <yiechertan@gmail.com>
2023-02-26 14:09:24 +0800

    URL submission and source detection. State management.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import '@fontsource/roboto/300.css';
@@ -6,16 +7,49 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SendIcon from '@mui/icons-material/Send';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [source, setSource] = useState('');
+  const [state, setState] = useState('input URL');  // input URL, retriving images
+
+  function submitUrl() {
+    if (url.includes('mangadex.org')) {
+      // get chapter hash
+      const mdUrl = url.split('/');
+      if (mdUrl.length >= 5) {
+        const chptHash = mdUrl[4];
+        console.log("Mangadex chapter hash: " + chptHash);
+        setState('retriving images');
+        setSource('Mangadex');
+      }
+    } else if (url.includes('rawkuma.com')) {
+      console.log("RawKuma chapter URL: " + url);
+      setState('retriving images');
+      setSource('Rawkuma');
+    }
+  }
+
   return (
-    <div className="App">
-      <div className="page-vertical justify-center">
+    <div className='app'>
+      <div className='page-vertical'>
+        <div style={{ height: '35%' }}/>
+        {/* form */}
         <div className='flex-container vertical-layout align-center pad'>
-          <TextField id="outlined-basic" label="URL" variant="outlined" className='fill-parent' />
-          <Typography variant="body1" gutterBottom>
-            Supported URLs: Mangadex, RawKuma
+          <TextField id='outlined-basic' label='Chapter URL' variant='outlined' className='fill-parent' onChange={(e) => setUrl(e.target.value)} />
+          <Typography variant='body1'>
+            Supported chapter URLs: Mangadex, RawKuma
           </Typography>
+          <LoadingButton disabled={!url || state != 'input URL'} variant='contained' style={{ margin: '16px' }}
+            loading={state == 'retriving images'} loadingPosition="end"
+            endIcon={<SendIcon />} onClick={submitUrl}>Submit</LoadingButton>
+          {state == 'retriving images' ? <div className='flex-container vertical-layout align-center'>
+          <Typography variant='h6'>{`${source} detected`}</Typography>
+          <Typography variant='caption'>retriving images...</Typography>
+          </div> : null}
         </div>
       </div>
     </div>
